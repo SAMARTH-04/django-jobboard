f6fb6506ccaf72ca7739e7426880614bd460d5e8
Samarth <samarthsr04@gmail.com>
2025-12-20 20:35:45 +0530

    fix Gmail SMTP sender authentication for status emails

--- applications/signals.py
+++ applications/signals.py
@@ -2,14 +2,34 @@ from django.db.models.signals import post_save
 from django.dispatch import receiver
 from django.core.mail import send_mail
 from .models import Application
+from django.conf import settings
 
 @receiver(post_save, sender=Application)
 def application_status_email(sender, instance, created, **kwargs):
-    if not created:
-        send_mail(
-            subject=f"Application {instance.status.capitalize()}",
-            message=f"Your application for {instance.job.title} is {instance.status}.",
-            from_email="noreply@jobboard.com",
-            recipient_list=[instance.applicant.email],
-            fail_silently=True,
+
+    # Send mail ONLY when status changes (not on create)
+    if not created and instance.applicant.email:
+
+        subject = f"Application {instance.status.capitalize()}"
+        message = (
+            f"Hello {instance.applicant.username},\n\n"
+            f"Your application for '{instance.job.title}' has been "
+            f"{instance.status.upper()}.\n\n"
+            "Best regards,\nJob Board Team"
         )
+
+        send_mail(
+            subject,
+            message,
+            settings.DEFAULT_FROM_EMAIL,
+            [instance.applicant.email],
+            fail_silently=False,
+        )
+        # testing SMTP
+        #     send_mail(
+        #     "SMTP Test",
+        #     "This confirms Gmail SMTP is working.",
+        #     settings.EMAIL_HOST_USER,
+        #     ["forstudies413@gmail.com"],  # ✅ REAL EMAIL
+        #     fail_silently=False,
+        # )
--- config/settings.py
+++ config/settings.py
@@ -11,10 +11,12 @@ https://docs.djangoproject.com/en/6.0/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+load_dotenv(BASE_DIR / '.env')
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/6.0/howto/deployment/checklist/
@@ -121,11 +123,22 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-EMAIL_BACKEND = "django.core.mail.backends.console.EmailBackend"
 LOGIN_REDIRECT_URL = '/accounts/dashboard/'
 LOGOUT_REDIRECT_URL = '/accounts/login/'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+EMAIL_HOST_USER = os.getenv('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = os.getenv('EMAIL_HOST_PASSWORD')
+
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
 
 
